4c8c217cac9bd25ae6e7d629d8183679b671b5a4
Habiba Akter Bilashee <wbilashee@gmail.com>
2022-11-12 14:02:41 +0600

    event loop done

---
+++ event-loop/read-file.js
@@ -0,0 +1,14 @@
+const { readFile } = require("fs");
+
+console.log("Started first task");
+
+readFile("../content/first.txt", "utf8", (err, result) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log(result);
+    console.log("Completed first task");
+});
+
+console.log("Starting next task");
---
+++ event-loop/setInterval.js
@@ -0,0 +1,4 @@
+setInterval(() => {
+    console.log("hello!");
+}, 2000);
+console.log("next task");
